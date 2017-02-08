ca9275950ec145dc9e9ad48234f95be8b94ea52b
Colette Smith <Colette@colettes-mbp.attlocal.net>
2017-02-08 15:03:27 -0500

    Created simple html framework

--- shop/index_page.py
+++ shop/index_page.py
@@ -1,5 +1,28 @@
 import requests
 
 def html():
-	lookbook = '<div class="lookbook">Lookbook One</div>'
-	return lookbook
+	return nav_bar() + lookbook()
+
+def lookbook():
+	return div('Lookbook One', "class='Lookbook'")
+
+def nav_bar():
+	list_items = [
+		li(a('Home', "href='/'")),
+		li(a('Products', "href='/products'"))
+	]
+	return ul(list_items, "class='NavBar")
+
+## Rad's Simple HTML Framework
+
+def div(content, attributes):
+	return '<div ' + attributes + '>' + content + '</div>'
+
+def a(content, attributes):
+	return '<a ' + attributes + '>' + content + '</a>'
+
+def li(content):
+	return '<li>' + content + '</li>'
+
+def ul(items, attributes):
+	return '<ul ' + attributes + '>' + ''.join(items) + '</ul>'
