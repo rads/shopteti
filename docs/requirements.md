e715226c42f63ca04f43b7f33b9c7699109d5ba5
Colette Smith <Colette@colettes-mbp.attlocal.net>
2017-02-08 13:24:57 -0500

    refactor shop.images

---
+++ docs/requirements.md
@@ -0,0 +1,7 @@
+# Requirements
+
+- Must have lookbook on homepage
+- Must have social media links on top
+- Must have separate product page
+- Must have toolbar at top
+- 
--- shop.py
+++ shop.py
@@ -3,11 +3,23 @@ import os
 ETSY_API_KEY = os.environ["ETSY_API_KEY"]
 
 def images():
-	r = requests.get('https://openapi.etsy.com/v2/shops/shopteti/listings/active?api_key='+ ETSY_API_KEY + '&includes=Images')
-	json = r.json()
+	json = get_data()
+	return format_data(json)
+
+def get_data():
+	response = requests.get('https://openapi.etsy.com/v2/shops/shopteti/listings/active?api_key='+ ETSY_API_KEY + '&includes=Images')
+	json = response.json()
+	return json
+
+def format_data(json):
 	results = json["results"]
 	image_tags = []
 	for listing in results:
 		image_url = listing["Images"][0]["url_170x135"]
 		image_tags.append("<img src=" + image_url + '>')
 	return "\n".join(image_tags)
+
+def debug(x):
+	file = open("/tmp/shopteti", 'w')
+	file.write(str(x))
+	file.close()
