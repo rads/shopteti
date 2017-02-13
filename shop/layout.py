8d618fec0be6e25d7a0649183f37c45ea4715b2a
Radford Smith <radsmith@gmail.com>
2017-02-12 17:28:13 -0800

    Add nav bar styles

--- shop/layout.py
+++ shop/layout.py
@@ -8,17 +8,10 @@ def html(content):
             stylesheets()
         ]),
         body('', [
-            logo(),
             nav_bar.html(),
             content
         ])
     ])
-
-def logo():
-    return h1('class="Logo"', a('href="/"', 'Teti'))
-
-def social_media_links():
-    return div('', 'social media links')
 
 def stylesheets():
     return link('rel="stylesheet" href="/static/style.css"')
--- shop/nav_bar.py
+++ shop/nav_bar.py
@@ -1,10 +1,21 @@
 from html import *
 
 def html():
-	return ul("class='NavBar'", [
-		home(),
+    return div('class="NavBar"', [
+        logo(),
+        menu(),
+        social_media_links()
+    ])
+
+def logo():
+    return h1('class="Logo"', a('href="/"', 'Teti'))
+
+def menu():
+	return ul("class='Menu'", [
+		li(a("href='/'", 'Home')),
 		li(a("href='/products'", 'Products'))
 	])
 
-def home():
-	return li(a("href='/'", 'Home'))
+def social_media_links():
+    return div('class="SocialMediaLinks"', 'social media links')
+
